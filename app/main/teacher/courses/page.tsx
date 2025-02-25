c2aca16cd3aae70fcd6d26df7198f106cab86d62
Chillibites <vitor.ribeiro.pvz@gmail.com>
2025-02-25 03:47:00 +0000

    added create page and New create button

--- app/main/teacher/courses/page.tsx
+++ app/main/teacher/courses/page.tsx
@@ -1,3 +1,15 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { Plus } from "lucide-react";
 export default function TeacherCoursesPage() {
-  return <div>TeacherCoursesPage</div>;
+  return (
+    <div className="p-6">
+      <Link href="/main/teacher/create">
+        <Button>
+          New Course
+          <Plus className="w-4 h-4 ml-2" />
+        </Button>
+      </Link>
+    </div>
+  );
 }
